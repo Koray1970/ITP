7d698dd4ff4f3164bcee22d5de5340bf496460f9
Koray Otağ <korayotag@gmail.com>
2023-10-20 14:35:59 +0300

    202310201435

--- app/src/main/java/com/isticpla/itp/signup/SignUpActivity.kt
+++ app/src/main/java/com/isticpla/itp/signup/SignUpActivity.kt
@@ -1,11 +1,9 @@
 package com.isticpla.itp.signup
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,30 +12,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.toggleable
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.ListItem
-import androidx.compose.material3.ListItemDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MenuDefaults
-import androidx.compose.material3.MenuItemColors
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -46,21 +30,15 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.Role
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.isticpla.itp.AppNavigate
 import com.isticpla.itp.R
-import com.isticpla.itp.dummydata.listofAraeCodes
 import com.isticpla.itp.signup.ui.theme.ITPTheme
+import dagger.hilt.android.qualifiers.ApplicationContext
 
 class SignUpActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -78,11 +56,27 @@ class SignUpActivity : ComponentActivity() {
         }
     }
 }
+lateinit var context:Context
+@Composable
+fun SingUpHeader(context: Context, title: String, subtitle: String) {
+    Text(
+        text = title,//context.getString(R.string.reg100),
+        style = signupHeader(context),
+        modifier = Modifier.fillMaxWidth()
+    )
+    Spacer(modifier = Modifier.height(40.dp))
+    Text(
+        text = subtitle,//context.getString(R.string.reg102),
+        style = signupSubTitle(context),
+        modifier = Modifier.fillMaxWidth()
+    )
+    Spacer(modifier = Modifier.height(80.dp))
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SignUp(navController: NavController) {
-    val context = LocalContext.current.applicationContext
+    context = LocalContext.current.applicationContext
     var phoneareaDropdownExpandState = remember { mutableStateOf(false) }
     var phoneNumberValue by remember { mutableStateOf("") }
     val (approveCheckedState, onStateChangeApprove) = remember { mutableStateOf(true) }
@@ -94,18 +88,11 @@ fun SignUp(navController: NavController) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = context.getString(R.string.reg100),
-            style = signupHeader(context),
-            modifier = Modifier.fillMaxWidth()
+        SingUpHeader(
+            context,
+            context.getString(R.string.reg100),
+            context.getString(R.string.reg102)
         )
-        Spacer(modifier = Modifier.height(40.dp))
-        Text(
-            text = context.getString(R.string.reg102),
-            style = signupSubTitle(context),
-            modifier = Modifier.fillMaxWidth()
-        )
-        Spacer(modifier = Modifier.height(80.dp))
         AreaPhoneTextField(modifier = Modifier, context)
         Spacer(modifier = Modifier.height(20.dp))
         Row(
@@ -123,7 +110,7 @@ fun SignUp(navController: NavController) {
             Checkbox(
                 checked = approveCheckedState,
                 onCheckedChange = null, // null recommended for accessibility with screenreaders
-                colors=signCheckBoxColors(context)
+                colors = signCheckBoxColors(context)
             )
             Text(
                 text = "Özel bildirim, güncelleme ve haberler hakkında tarafımla e-posta ve SMS ile iletişime geçilmesini istiyorum.",
@@ -147,7 +134,7 @@ fun SignUp(navController: NavController) {
             Checkbox(
                 checked = pcontractCheckedState,
                 onCheckedChange = null, // null recommended for accessibility with screenreaders
-                colors=signCheckBoxColors(context)
+                colors = signCheckBoxColors(context)
             )
             Text(
                 text = "Üyelik Sözleşmesini okudum ve kabul ediyorum.",
@@ -184,7 +171,20 @@ fun SignUp(navController: NavController) {
 
 @Composable
 fun VerifyPhoneNumber(navController: NavController) {
-
+    context=LocalContext.current.applicationContext
+    Scaffold() { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            SingUpHeader(
+                context,
+                context.getString(R.string.reg200),
+                String.format(context.getString(R.string.reg201),"+905336221493")
+            )
+        }
+    }
 }
 
 @Composable
