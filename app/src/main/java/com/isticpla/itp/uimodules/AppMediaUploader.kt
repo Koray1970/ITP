17a379adc8ca8e968ca4332d2f146fed5ee899bc
Koray Otağ <korayotag@gmail.com>
2023-11-19 13:41:21 +0300

    202311191340

--- app/src/main/java/com/isticpla/itp/uimodules/AppMediaUploader.kt
+++ app/src/main/java/com/isticpla/itp/uimodules/AppMediaUploader.kt
@@ -2,7 +2,9 @@ package com.isticpla.itp.uimodules
 
 import android.Manifest.permission.CAMERA
 import android.content.Context
+import android.content.Context.MODE_PRIVATE
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.icu.text.SimpleDateFormat
 import android.net.Uri
 import android.widget.Toast
@@ -17,16 +19,22 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeightIn
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -49,13 +57,16 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
+import coil.compose.AsyncImage
 import coil.compose.rememberAsyncImagePainter
 import coil.compose.rememberImagePainter
 import com.isticpla.itp.R
 import com.isticpla.itp.poppinFamily
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.launch
 import java.io.File
+import java.io.IOException
 import java.util.Date
 import java.util.Objects
 
@@ -64,17 +75,27 @@ import java.util.Objects
 @Composable
 fun AppMediaUploader() {
     val context = LocalContext.current.applicationContext
-
+    val scope= rememberCoroutineScope()
 
     val file = context.createImageFile()
+
     val uri = FileProvider.getUriForFile(
         Objects.requireNonNull(context),
         context.packageName + ".provider", file
     )
-    var capturedImageUri = remember { mutableStateListOf<Uri>() }
+    var capturedImageUri = remember { mutableStateListOf<Uri?>(null) }
     val cameraLauncher =
-        rememberLauncherForActivityResult(ActivityResultContracts.TakePicture()) {
+        rememberLauncherForActivityResult(ActivityResultContracts.TakePicturePreview()) {
             capturedImageUri.add(uri)
+            scope.launch {
+                saveImageToInternalStorage(context, uri, file.name)
+            }
+        }
+    val galleryLauncher =
+        rememberLauncherForActivityResult(ActivityResultContracts.GetMultipleContents()) {
+            it.forEach {
+                capturedImageUri.add(it)
+            }
         }
 
     val permissionLauncher = rememberLauncherForActivityResult(
@@ -89,7 +110,7 @@ fun AppMediaUploader() {
     }
     val textMeasurer = rememberTextMeasurer()
     Column(
-        verticalArrangement = Arrangement.spacedBy(10.dp,Alignment.CenterVertically),
+        verticalArrangement = Arrangement.spacedBy(10.dp, Alignment.CenterVertically),
         horizontalAlignment = Alignment.Start
     ) {
         //region media add buttons
@@ -121,7 +142,9 @@ fun AppMediaUploader() {
                 Text(text = "Kamera")
             }
             Button(
-                onClick = {},
+                onClick = {
+                    galleryLauncher.launch("image/*")
+                },
                 shape = RoundedCornerShape(6.dp),
                 colors = ButtonDefaults.buttonColors(
                     containerColor = AppColors.blue_102,
@@ -155,8 +178,8 @@ fun AppMediaUploader() {
                         textMeasurer.measure(
                             AnnotatedString(text = "...Ürün Görselleri..."),
                             constraints = Constraints.fixed(
-                                width =(size.width/2f).toInt(),
-                                height = (size.height/2f).toInt()
+                                width = (size.width / 2f).toInt(),
+                                height = (size.height / 2f).toInt()
                             ),
                             style = TextStyle(
                                 fontFamily = poppinFamily,
@@ -165,34 +188,64 @@ fun AppMediaUploader() {
                                 color = AppColors.primaryGreyDisabled
                             )
                         )
-                    drawText(measuredText, topLeft = Offset(x=(size.width/6.4f).dp.toPx(),y=(size.height/5).dp.toPx()))
+                    drawText(
+                        measuredText,
+                        topLeft = Offset(
+                            x = (size.width / 6.4f).dp.toPx(),
+                            y = (size.height / 5).dp.toPx()
+                        )
+                    )
                 },
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(10.dp,Alignment.CenterHorizontally)
-        ) {
-            if (capturedImageUri.isNotEmpty()) {
-                capturedImageUri.forEach { img ->
+
+            ) {
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(5)
+            ) {
+                items(capturedImageUri.size) { index ->
                     Image(
                         modifier = Modifier
                             .size(70.dp),
-                        painter = rememberAsyncImagePainter(img),
+                        painter = rememberAsyncImagePainter(capturedImageUri.get(index)),
                         contentDescription = null
                     )
                 }
             }
         }
-
     }
 }
 
-fun Context.createImageFile(): File {
+private fun Context.createImageFile(): File {
     // Create an image file name
     val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-    val imageFileName = "JPEG_" + timeStamp + "_"
+    val imageFileName = "itp_" + timeStamp + "_"
     val image = File.createTempFile(
         imageFileName, /* prefix */
         ".jpg", /* suffix */
         externalCacheDir      /* directory */
     )
     return image
+}
+
+private fun saveImageToInternalStorage(context: Context, uri: Uri, filename: String) {
+    val inputStream = context.contentResolver.openInputStream(uri)
+    val outputStream = context.openFileOutput(filename, Context.MODE_PRIVATE)
+    inputStream?.use { input ->
+        outputStream.use { output ->
+            input.copyTo(output)
+        }
+    }
+}
+
+private fun savePhotoToInternalStorage(context: Context,filename: String, bmp: Bitmap): Boolean {
+    return try {
+        context.openFileOutput("$filename.jpg", MODE_PRIVATE).use { stream ->
+            if(!bmp.compress(Bitmap.CompressFormat.JPEG, 95, stream)) {
+                throw IOException("Couldn't save bitmap.")
+            }
+        }
+        true
+    } catch(e: IOException) {
+        e.printStackTrace()
+        false
+    }
 }
