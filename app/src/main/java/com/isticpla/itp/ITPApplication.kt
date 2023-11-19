1a795caec208fdf63c30ed58a578f2c4b169da10
Koray Otağ <korayotag@gmail.com>
2023-11-20 01:11:55 +0300

    202311200110

--- app/src/main/java/com/isticpla/itp/ITPApplication.kt
+++ app/src/main/java/com/isticpla/itp/ITPApplication.kt
@@ -1,8 +1,12 @@
 package com.isticpla.itp
 
 import android.app.Application
+import android.util.Log
+import androidx.camera.camera2.Camera2Config
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.CameraXConfig
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class ITPApplication: Application() {
+class ITPApplication : Application() {
 }
--- app/src/main/java/com/isticpla/itp/uimodules/AppMediaUploader.kt
+++ app/src/main/java/com/isticpla/itp/uimodules/AppMediaUploader.kt
@@ -1,35 +1,63 @@
 package com.isticpla.itp.uimodules
 
 import android.Manifest.permission.CAMERA
+import android.content.ContentResolver
+import android.content.ContentValues
 import android.content.Context
 import android.content.Context.MODE_PRIVATE
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.Matrix
 import android.icu.text.SimpleDateFormat
 import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.CameraController
+import androidx.camera.view.LifecycleCameraController
+import androidx.camera.view.PreviewView
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeightIn
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.BottomSheetScaffoldState
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
@@ -38,13 +66,16 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
@@ -52,45 +83,111 @@ import androidx.compose.ui.text.drawText
 import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Constraints
-import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getString
 import androidx.core.content.FileProvider
-import coil.compose.AsyncImage
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberAsyncImagePainter
-import coil.compose.rememberImagePainter
+import com.isticpla.itp.BuildConfig
 import com.isticpla.itp.R
 import com.isticpla.itp.poppinFamily
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOf
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import java.io.File
+import java.io.FileOutputStream
 import java.io.IOException
+import java.io.OutputStream
 import java.util.Date
+import java.util.Locale
 import java.util.Objects
+import java.util.concurrent.Executor
+import javax.inject.Inject
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
+@Composable
+fun CameraPreview(
+    controller: LifecycleCameraController,
+    modifier: Modifier = Modifier
+) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    AndroidView(
+        factory = {
+            PreviewView(it).apply {
+                this.controller = controller
+                controller.bindToLifecycle(lifecycleOwner)
+            }
+        },
+        modifier = modifier
+    )
+}
 
-@OptIn(ExperimentalLayoutApi::class)
+@Composable
+fun PhotoBottomSheetContent(
+    bitmaps: List<Bitmap>,
+    modifier: Modifier = Modifier
+) {
+    if (bitmaps.isEmpty()) {
+        Box(
+            modifier = modifier
+                .padding(16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text("Yüklü görsel yok!")
+        }
+    } else {
+        LazyVerticalStaggeredGrid(
+            columns = StaggeredGridCells.Fixed(2),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalItemSpacing = 16.dp,
+            contentPadding = PaddingValues(16.dp),
+            modifier = modifier
+        ) {
+            items(bitmaps) { bitmap ->
+                Image(
+                    bitmap = bitmap.asImageBitmap(),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(10.dp))
+                )
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalLayoutApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun AppMediaUploader() {
     val context = LocalContext.current.applicationContext
-    val scope= rememberCoroutineScope()
+    val scope = rememberCoroutineScope()
+    //val cameraViewModel = viewModel<CameraViewModel>()
+    val cameraViewModel = hiltViewModel<CameraViewModel>()
 
-    val file = context.createImageFile()
-
-    val uri = FileProvider.getUriForFile(
-        Objects.requireNonNull(context),
-        context.packageName + ".provider", file
-    )
-    var capturedImageUri = remember { mutableStateListOf<Uri?>(null) }
-    val cameraLauncher =
-        rememberLauncherForActivityResult(ActivityResultContracts.TakePicturePreview()) {
-            capturedImageUri.add(uri)
-            scope.launch {
-                saveImageToInternalStorage(context, uri, file.name)
-            }
+    var cameraViewerState = rememberBottomSheetScaffoldState()
+    val bitmaps by cameraViewModel.bitmaps.collectAsState()
+    val controller = remember {
+        LifecycleCameraController(context).apply {
+            setEnabledUseCases(
+                CameraController.IMAGE_CAPTURE or
+                        CameraController.VIDEO_CAPTURE
+            )
         }
+    }
+    controller.cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+    /*if (controller.cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA) {
+            CameraSelector.DEFAULT_FRONT_CAMERA
+        } else CameraSelector.DEFAULT_BACK_CAMERA*/
+    var capturedImageUri = remember { mutableStateListOf<Uri?>(null) }
+
     val galleryLauncher =
         rememberLauncherForActivityResult(ActivityResultContracts.GetMultipleContents()) {
             it.forEach {
@@ -103,12 +200,18 @@ fun AppMediaUploader() {
     ) {
         if (it) {
             Toast.makeText(context, "Permission Granted", Toast.LENGTH_SHORT).show()
-            cameraLauncher.launch(uri)
+            scope.launch {
+                cameraViewerState.bottomSheetState.expand()
+            }
+            //cameraLauncher.launch(tempPhotoUri )
         } else {
             Toast.makeText(context, "Permission Denied", Toast.LENGTH_SHORT).show()
         }
     }
     val textMeasurer = rememberTextMeasurer()
+
+
+
     Column(
         verticalArrangement = Arrangement.spacedBy(10.dp, Alignment.CenterVertically),
         horizontalAlignment = Alignment.Start
@@ -123,7 +226,9 @@ fun AppMediaUploader() {
                     val permissionCheckResult =
                         ContextCompat.checkSelfPermission(context, CAMERA)
                     if (permissionCheckResult == PackageManager.PERMISSION_GRANTED) {
-                        cameraLauncher.launch(uri)
+                        scope.launch {
+                            cameraViewerState.bottomSheetState.partialExpand()
+                        }
                     } else {
                         // Request a permission
                         permissionLauncher.launch(CAMERA)
@@ -212,40 +317,207 @@ fun AppMediaUploader() {
             }
         }
     }
+    BottomSheetScaffold(
+        scaffoldState = cameraViewerState,
+        sheetPeekHeight = 0.dp,
+        sheetContent = {
+            PhotoBottomSheetContent(
+                bitmaps = bitmaps,
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+        }
+    ) { padding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+        ) {
+            CameraPreview(
+                controller = controller,
+                modifier = Modifier
+                    .fillMaxSize()
+            )
+            IconButton(
+                onClick = {
+                    controller.cameraSelector =
+                        if (controller.cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA) {
+                            CameraSelector.DEFAULT_FRONT_CAMERA
+                        } else CameraSelector.DEFAULT_BACK_CAMERA
+                },
+                modifier = Modifier
+                    .offset(16.dp, 16.dp)
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.baseline_cameraswitch_24),
+                    contentDescription = null
+                )
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.BottomCenter)
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.SpaceAround
+            ) {
+                IconButton(
+                    onClick = {
+                        scope.launch {
+                            cameraViewerState.bottomSheetState.expand()
+                        }
+                    }
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.outline_photo_library_24),
+                        contentDescription = null
+                    )
+                }
+                IconButton(
+                    onClick = {
+                        takePhoto(
+                            context = context,
+                            cameraviewerstate = cameraViewerState,
+                            controller = controller,
+                            onPhotoTaken = cameraViewModel::onTakePhoto
+                        )
+                    }
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.outline_camera_alt_24),
+                        contentDescription = null
+                    )
+                }
+            }
+        }
+    }
+
 }
 
-private fun Context.createImageFile(): File {
-    // Create an image file name
-    val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-    val imageFileName = "itp_" + timeStamp + "_"
-    val image = File.createTempFile(
-        imageFileName, /* prefix */
-        ".jpg", /* suffix */
-        externalCacheDir      /* directory */
+@OptIn(ExperimentalMaterial3Api::class)
+private fun takePhoto(
+    context: Context,
+    cameraviewerstate: BottomSheetScaffoldState,
+    controller: LifecycleCameraController,
+    onPhotoTaken: (Bitmap) -> Unit
+) {
+    controller.takePicture(
+        ContextCompat.getMainExecutor(context.applicationContext),
+        object : ImageCapture.OnImageCapturedCallback() {
+            override fun onCaptureSuccess(image: ImageProxy) {
+                super.onCaptureSuccess(image)
+
+                val matrix = Matrix().apply {
+                    postRotate(image.imageInfo.rotationDegrees.toFloat())
+                }
+                val rotatedBitmap = Bitmap.createBitmap(
+                    image.toBitmap(),
+                    0,
+                    0,
+                    image.width,
+                    image.height,
+                    matrix,
+                    true
+                )
+
+                onPhotoTaken(rotatedBitmap)
+
+                MainScope().launch {
+                    delay(200)
+                    saveBitmapImage(context,rotatedBitmap)
+                    //cameraviewerstate.bottomSheetState.partialExpand()
+                }
+            }
+
+            override fun onError(exception: ImageCaptureException) {
+                super.onError(exception)
+                Log.e("Camera", "Couldn't take photo: ", exception)
+                /*MainScope().launch {
+                    delay(200)
+                    cameraviewerstate.bottomSheetState.partialExpand()
+                }*/
+            }
+        }
     )
-    return image
 }
 
-private fun saveImageToInternalStorage(context: Context, uri: Uri, filename: String) {
+
+/**Save Bitmap To Gallery
+ * @param bitmap The bitmap to be saved in Storage/Gallery*/
+private fun saveBitmapImage(context:Context,bitmap: Bitmap) {
+    val TAG="saveBitmapImage"
+    val timestamp = System.currentTimeMillis()
+
+    //Tell the media scanner about the new file so that it is immediately available to the user.
+    val values = ContentValues()
+    val contentResolver=context.contentResolver
+    values.put(MediaStore.Images.Media.MIME_TYPE, "image/png")
+    values.put(MediaStore.Images.Media.DATE_ADDED, timestamp)
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        values.put(MediaStore.Images.Media.DATE_TAKEN, timestamp)
+        values.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/" + context.getString(R.string.app_name))
+        values.put(MediaStore.Images.Media.IS_PENDING, true)
+        val uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+        if (uri != null) {
+            try {
+                val outputStream = contentResolver.openOutputStream(uri)
+                if (outputStream != null) {
+                    try {
+                        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+                        outputStream.close()
+                    } catch (e: Exception) {
+                        Log.e(TAG, "saveBitmapImage: ", e)
+                    }
+                }
+                values.put(MediaStore.Images.Media.IS_PENDING, false)
+                contentResolver.update(uri, values, null, null)
+
+                Toast.makeText(context, "Saved...", Toast.LENGTH_SHORT).show()
+            } catch (e: Exception) {
+                Log.e(TAG, "saveBitmapImage: ", e)
+            }
+        }
+    } else {
+        val imageFileFolder = File(Environment.getExternalStorageDirectory().toString() + '/' + context.getString(R.string.app_name))
+        if (!imageFileFolder.exists()) {
+            imageFileFolder.mkdirs()
+        }
+        val mImageName = "$timestamp.png"
+        val imageFile = File(imageFileFolder, mImageName)
+        try {
+            val outputStream: OutputStream = FileOutputStream(imageFile)
+            try {
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+                outputStream.close()
+            } catch (e: Exception) {
+                Log.e(TAG, "saveBitmapImage: ", e)
+            }
+            values.put(MediaStore.Images.Media.DATA, imageFile.absolutePath)
+            contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+
+            Toast.makeText(context, "Saved...", Toast.LENGTH_SHORT).show()
+        } catch (e: Exception) {
+            Log.e(TAG, "saveBitmapImage: ", e)
+        }
+    }
+}
+
+private fun saveImageToInternalStorage(context: Context, uri: Uri) {
     val inputStream = context.contentResolver.openInputStream(uri)
-    val outputStream = context.openFileOutput(filename, Context.MODE_PRIVATE)
+    val outputStream = context.openFileOutput("image.jpg", Context.MODE_PRIVATE)
     inputStream?.use { input ->
         outputStream.use { output ->
             input.copyTo(output)
         }
     }
 }
+@HiltViewModel
+class CameraViewModel @Inject constructor() : ViewModel() {
 
-private fun savePhotoToInternalStorage(context: Context,filename: String, bmp: Bitmap): Boolean {
-    return try {
-        context.openFileOutput("$filename.jpg", MODE_PRIVATE).use { stream ->
-            if(!bmp.compress(Bitmap.CompressFormat.JPEG, 95, stream)) {
-                throw IOException("Couldn't save bitmap.")
-            }
-        }
-        true
-    } catch(e: IOException) {
-        e.printStackTrace()
-        false
+    private val _bitmaps = MutableStateFlow<List<Bitmap>>(emptyList())
+    val bitmaps = _bitmaps.asStateFlow()
+
+    fun onTakePhoto(bitmap: Bitmap) {
+        _bitmaps.value += bitmap
     }
 }
