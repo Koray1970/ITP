907dec0d31ad66d8249c6e17c9b8a46ff9601e37
Koray Otağ <korayotag@gmail.com>
2023-11-27 15:29:12 +0300

    202311271528

--- app/src/main/java/com/isticpla/itp/offerdetails/OfferViewModel.kt
+++ app/src/main/java/com/isticpla/itp/offerdetails/OfferViewModel.kt
@@ -1,13 +1,24 @@
 package com.isticpla.itp.offerdetails
 
+import android.content.ContentResolver
+import android.content.ContentUris
+import android.content.Context
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.isticpla.itp.dummydata.ProductFeatureItem
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.launch
 import java.util.UUID
@@ -17,14 +28,64 @@ data class AdditionalProductDetails(
     val id: UUID = UUID.randomUUID(),
     val formitem: ProductFeatureItem,
     val txtvalue: MutableState<String> = mutableStateOf(""),
-    var txtiserror:MutableState<Boolean> = mutableStateOf(false)
+    var txtiserror: MutableState<Boolean> = mutableStateOf(false)
 )
 
 @HiltViewModel
-class OfferViewModel @Inject constructor() : ViewModel() {
+class OfferViewModel @Inject constructor(@ApplicationContext private val context: Context) :
+    ViewModel() {
     private var listofAdditionalProductDetail = mutableStateListOf<AdditionalProductDetails>()
     val additionalProductDetails =
         flowOf<MutableList<AdditionalProductDetails>>(listofAdditionalProductDetail)
+    private var listofMediaFiles= context.fileList().toMutableList().toMutableStateList()
+    val getFileFromLocalDir: Flow<MutableList<String>> = flow{
+        while (true){
+            emit(context.fileList().sortedDescending().toMutableStateList())
+            delay(1200L)
+        }
+    }
+    val getFileFromSharedDir: Flow<MutableList<Uri>> = flow{
+        while (true){
+            val collection=
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    MediaStore.Images.Media.getContentUri(
+                        MediaStore.VOLUME_EXTERNAL
+                    )
+                } else {
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                }
+            val projection = arrayOf(
+                MediaStore.Images.Media._ID,
+                MediaStore.Images.Media.DISPLAY_NAME,
+                MediaStore.Images.Media.DATE_ADDED,
+                MediaStore.Images.Media.MIME_TYPE,
+                MediaStore.Images.Media.SIZE
+            )
+            val selection = "${MediaStore.Images.Media.MIME_TYPE} = ?"
+            val selectionArgs = arrayOf("image/jpeg","image/x-png")
+
+            val sortOrder = "${MediaStore.Images.Media.DATE_ADDED} ASC"
+            val query = context.contentResolver.query(
+                collection,
+                projection,
+                selection,
+                selectionArgs,
+                sortOrder
+            )
+
+            query?.use{cursor->
+                val listofimage= mutableListOf<Uri>()
+                while (cursor.moveToNext()){
+                    listofimage.add(ContentUris.withAppendedId(
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Video.Media._ID))
+                    ))
+                }
+                emit(listofimage)
+            }
+
+            delay(1200L)
+        }
+    }
 
     fun AddItemToAdditionalProductDetails(itm: AdditionalProductDetails) = viewModelScope.launch {
         listofAdditionalProductDetail.add(itm)
@@ -34,4 +95,5 @@ class OfferViewModel @Inject constructor() : ViewModel() {
         viewModelScope.launch {
             listofAdditionalProductDetail.remove(itm)
         }
+
 }
--- app/src/main/java/com/isticpla/itp/offers/CreateOfferVisualDetails.kt
+++ app/src/main/java/com/isticpla/itp/offers/CreateOfferVisualDetails.kt
@@ -9,15 +9,17 @@ import android.os.Build
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.result.launch
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -25,37 +27,46 @@ import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SheetState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import coil.ImageLoader
+import coil.compose.AsyncImage
 import coil.request.ErrorResult
 import coil.request.ImageRequest
 import coil.request.SuccessResult
 import com.isticpla.itp.BuildConfig
 import com.isticpla.itp.R
+import com.isticpla.itp.offerdetails.OfferViewModel
 import com.isticpla.itp.uimodules.AppColors
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import java.io.File
 import java.text.SimpleDateFormat
@@ -63,46 +74,114 @@ import java.util.Date
 import java.util.Objects
 
 @RequiresApi(Build.VERSION_CODES.TIRAMISU)
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
 @Composable
 fun CreateOfferVisualDetails(
     navController: NavController
 ) {
     val radius = 6.dp
-    var captureImageUri = remember {
-        mutableStateOf<Uri>(Uri.EMPTY)
-    }
+    val context = LocalContext.current.applicationContext
+    val offerViewModel = hiltViewModel<OfferViewModel>()
+    val scope = rememberCoroutineScope()
+    val showGalleryBottomSheet = remember { mutableStateOf(false) }
+    var gallerySheetState = rememberModalBottomSheetState()
+
+    val imagelist =
+        offerViewModel.getFileFromLocalDir.collectAsStateWithLifecycle(initialValue = emptyList<String>().toMutableStateList())
+
+    val sharedGalleryImageList = offerViewModel.getFileFromSharedDir.collectAsStateWithLifecycle(
+        initialValue = emptyList<Uri>().toMutableStateList()
+    )
+
+
+
     BottomSheetScaffold(
         containerColor = Color.White,
         topBar = {
-            TopAppBar(
-                colors = TopAppBarColors(
-                    containerColor = Color.White, scrolledContainerColor = Color.White,
-                    navigationIconContentColor = AppColors.primaryGrey,
-                    titleContentColor = AppColors.primaryGrey,
-                    actionIconContentColor = AppColors.primaryGrey,
-                ),
-                title = { Text("Teklif Talebi Oluştur", style = offerTopBarTitle) },
-                actions = {
-                    IconButton(onClick = { navController.navigate("home") }) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.outline_home_24),
-                            contentDescription = null
-                        )
-                    }
-                },
-                navigationIcon = {
-                    IconButton(onClick = { navController.navigate("offer/create") }) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.arrow_left),
-                            contentDescription = null
-                        )
-                    }
+            TopAppBar(colors = TopAppBarColors(
+                containerColor = Color.White, scrolledContainerColor = Color.White,
+                navigationIconContentColor = AppColors.primaryGrey,
+                titleContentColor = AppColors.primaryGrey,
+                actionIconContentColor = AppColors.primaryGrey,
+            ), title = { Text("Teklif Talebi Oluştur", style = offerTopBarTitle) }, actions = {
+                IconButton(onClick = { navController.navigate("home") }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.outline_home_24),
+                        contentDescription = null
+                    )
                 }
-            )
+            }, navigationIcon = {
+                IconButton(onClick = { navController.navigate("offer/create") }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.arrow_left),
+                        contentDescription = null
+                    )
+                }
+            })
         },
-        sheetContent = {}
-    ) { innerpadding ->
+        sheetPeekHeight = 0.dp,
+        sheetContent = {
+            if (showGalleryBottomSheet.value) {
+                ModalBottomSheet(
+                    onDismissRequest = {
+                        showGalleryBottomSheet.value = false
+                    },
+                    sheetState = gallerySheetState
+                ) {
+
+                    if (imagelist.value.isNotEmpty()) {
+                        FlowRow(
+                            modifier = Modifier.fillMaxSize(),
+                            horizontalArrangement = Arrangement.Start,
+                            verticalArrangement = Arrangement.spacedBy(
+                                10.dp,
+                                Alignment.Top
+                            ),
+                            maxItemsInEachRow = 3
+                        ) {
+                            imagelist.value.filter { a -> a.contains(".jpg") }.forEach { img ->
+                                AsyncImage(
+                                    modifier = Modifier.size(140.dp),
+                                    model = "${context.filesDir}/${img}",
+                                    contentDescription = null,
+                                    contentScale = ContentScale.Fit
+                                )
+                            }
+                        }
+                    }
+                    HorizontalDivider(thickness = 1.dp, color = Color.Red)
+                    if (sharedGalleryImageList.value.isNotEmpty()) {
+                        FlowRow(
+                            modifier = Modifier.fillMaxSize(),
+                            horizontalArrangement = Arrangement.Start,
+                            verticalArrangement = Arrangement.spacedBy(
+                                10.dp,
+                                Alignment.Top
+                            ),
+                            maxItemsInEachRow = 3
+                        ) {
+                            sharedGalleryImageList.value.forEach { img ->
+                                AsyncImage(
+                                    modifier = Modifier.size(140.dp),
+                                    model = img,
+                                    contentDescription = null,
+                                    contentScale = ContentScale.Fit
+                                )
+                            }
+                        }
+                    }
+
+                    // Sheet content
+                    /*Button(onClick = {
+                    scope.launch { gallerySheetState.hide() }.invokeOnCompletion {
+
+                    }
+                }) {
+
+                }*/
+                }
+            }
+        }) { innerpadding ->
         Column(
             modifier = Modifier
                 .padding(innerpadding)
@@ -118,13 +197,24 @@ fun CreateOfferVisualDetails(
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.spacedBy(10.dp, Alignment.Start)
             ) {
-                UseCamera(captureImageUri = captureImageUri, radius = radius)
+                UseCamera(
+                    radius = radius,
+                    offerViewModel,
+                    showGalleryBottomSheet,
+                    mutableStateOf(gallerySheetState)
+                )
                 Button(
-                    onClick = {},
+                    onClick = {
+                        scope.launch {
+                            //offerViewModel.ReloadMediaFiles()
+                            delay(200L)
+                            showGalleryBottomSheet.value = true
+                            gallerySheetState.show()
+                        }
+                    },
                     shape = RoundedCornerShape(radius),
                     colors = ButtonDefaults.buttonColors(
-                        containerColor = AppColors.blue_0xFF0495f1,
-                        contentColor = Color.White
+                        containerColor = AppColors.blue_0xFF0495f1, contentColor = Color.White
                     )
                 ) {
                     Icon(
@@ -138,49 +228,55 @@ fun CreateOfferVisualDetails(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @RequiresApi(Build.VERSION_CODES.TIRAMISU)
 @Composable
 fun UseCamera(
-    captureImageUri: MutableState<Uri> = mutableStateOf<Uri>(Uri.EMPTY),
-    radius: Dp
+    radius: Dp,
+    offerViewModel: OfferViewModel,
+    showGalleryBottomSheet: MutableState<Boolean> = mutableStateOf(false),
+    sheetState: MutableState<SheetState>
 ) {
     val context = LocalContext.current
-    val scope= rememberCoroutineScope()
-    /*var file = context.createImageFile(context)
+    val scope = rememberCoroutineScope()
+    var file = context.createImageFile()
     var uri = FileProvider.getUriForFile(
         Objects.requireNonNull(context), BuildConfig.APPLICATION_ID + ".provider", file
-    )*/
+    )
 
     val cameraLauncher =
-        rememberLauncherForActivityResult(contract = ActivityResultContracts.TakePicturePreview()) {
+        rememberLauncherForActivityResult(contract = ActivityResultContracts.TakePicture()) {
+            if (it != null) {
 
-            if(it!=null) {
-                val file = context.createImageFile(context)
                 //captureImageUri.value = uri
-                var bitmap: Bitmap? = it
-                bitmap?.compress(Bitmap.CompressFormat.JPEG, 100, file.outputStream())
+                var bitmap: Bitmap?
 
-
-                /*val loadBitmap = scope.launch(Dispatchers.IO) {
+                val loadBitmap = scope.launch(Dispatchers.IO) {
                     val loader = ImageLoader(context)
-                    val request = ImageRequest.Builder(context)
-                        .data(uri)
-                        .allowHardware(false)
-                        .build()
+                    val request =
+                        ImageRequest.Builder(context).data(uri).allowHardware(false).build()
                     val result = loader.execute(request)
                     if (result is SuccessResult) {
                         bitmap = (result.drawable as BitmapDrawable).bitmap
-                        bitmap?.compress(Bitmap.CompressFormat.JPEG, 100, file.outputStream())
+                        bitmap?.compress(
+                            Bitmap.CompressFormat.JPEG,
+                            100,
+                            context.createImageFileToLocalDirectory().outputStream()
+                        )
+                        //offerViewModel.ReloadMediaFiles()
+                        delay(250L)
+                        showGalleryBottomSheet.value = true
+                        sheetState.value.show()
                     } else if (result is ErrorResult) {
                         cancel(result.throwable.localizedMessage ?: "ErrorResult", result.throwable)
                     }
-                }*/
+                }
             }
         }
     val permissionLauncher =
         rememberLauncherForActivityResult(contract = ActivityResultContracts.RequestPermission()) {
             if (it) {
-                cameraLauncher.launch()
+                cameraLauncher.launch(uri)
             } else {
                 Toast.makeText(context, "Permission Denied", Toast.LENGTH_SHORT).show()
             }
@@ -190,16 +286,13 @@ fun UseCamera(
             val permissionCheckResult =
                 ContextCompat.checkSelfPermission(context, android.Manifest.permission.CAMERA)
             if (permissionCheckResult == PackageManager.PERMISSION_GRANTED) {
-                cameraLauncher.launch()
+                cameraLauncher.launch(uri)
             } else {
                 // Request a permission
                 permissionLauncher.launch(android.Manifest.permission.CAMERA)
             }
-        },
-        shape =  RoundedCornerShape(radius),
-        colors = ButtonDefaults.buttonColors(
-            containerColor = AppColors.blue_0xFF0495f1,
-            contentColor = Color.White
+        }, shape = RoundedCornerShape(radius), colors = ButtonDefaults.buttonColors(
+            containerColor = AppColors.blue_0xFF0495f1, contentColor = Color.White
         )
     ) {
         Icon(
@@ -211,7 +304,12 @@ fun UseCamera(
 }
 
 
-fun Context.createImageFile(context: Context): File {
+fun Context.createImageFile(): File {
     val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-    return File.createTempFile(timeStamp, ".jpg", context.filesDir)
+    return File.createTempFile(timeStamp, ".jpg", externalCacheDir)
+}
+
+fun Context.createImageFileToLocalDirectory(): File {
+    val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+    return File.createTempFile(timeStamp, ".jpg", filesDir)
 }
