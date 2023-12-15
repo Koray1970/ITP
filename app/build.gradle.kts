2098ab5974fa4fb8d0aa3b6e5cf57558a6f53a46
Koray Otağ <korayotag@gmail.com>
2023-12-16 00:43:54 +0300

    202312152355

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,19 +70,19 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3:1.2.0-alpha11")
+    implementation("androidx.compose.material3:material3:1.2.0-alpha12")
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation("androidx.navigation:navigation-compose:2.7.5")
     implementation("io.coil-kt:coil-compose:2.5.0")
-    implementation("androidx.compose.ui:ui-text-google-fonts:1.6.0-beta01")
+    implementation("androidx.compose.ui:ui-text-google-fonts:1.6.0-beta02")
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("io.coil-kt:coil-compose:2.5.0")
     implementation("com.github.bumptech.glide:compose:1.0.0-beta01")
-    implementation("androidx.compose.foundation:foundation-layout:1.6.0-beta01")
-    implementation("androidx.compose.foundation:foundation-layout-android:1.6.0-beta01")
+    implementation("androidx.compose.foundation:foundation-layout:1.6.0-beta02")
+    implementation("androidx.compose.foundation:foundation-layout-android:1.6.0-beta02")
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
@@ -112,10 +112,10 @@ dependencies {
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.01"))
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.01"))
     debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.0-beta01")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.0-beta02")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
-    implementation("com.google.dagger:hilt-android:2.48.1")
-    kapt("com.google.dagger:hilt-android-compiler:2.48.1")
+    implementation("com.google.dagger:hilt-android:2.49")
+    kapt("com.google.dagger:hilt-android-compiler:2.49")
     // When using Kotlin.
     kapt("androidx.hilt:hilt-compiler:1.1.0")
     // When using Java.
@@ -139,7 +139,7 @@ dependencies {
     implementation("androidx.camera:camera-extensions:${camerax_version}")
     //endregion
     //region room database dependecy
-    val room_version = "2.6.0"
+    val room_version = "2.6.1"
     implementation("androidx.room:room-runtime:$room_version")
     annotationProcessor("androidx.room:room-compiler:$room_version")
     // To use Kotlin annotation processing tool (kapt)
